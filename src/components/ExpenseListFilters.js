f0cdfd40a329ef40f0824908dfc3eafe0855642a
Fabio <heloflyer@hotmail.com>
2019-01-01 18:55:37 -0800

    created expense add-edit form, added datepicker

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters'
+import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters'
 
 const ExpenseListFilters = (props) => (
     <div>
@@ -11,6 +11,21 @@ const ExpenseListFilters = (props) => (
                 props.dispatch(setTextFilter(e.target.value));
             }}
         />
+        <select 
+            type="text"
+            value={props.filters.sortBy}
+            onChange={(e) => {
+                if ( e.target.value === "amount" ) {
+                    props.dispatch(sortByAmount());
+                }
+                else if ( e.target.value === "date" ) {
+                    props.dispatch(sortByDate());
+                }
+            }}
+        >
+        <option value="date">Date</option>
+        <option value="amount">Amount</option>
+        </select>
     </div>
 
 );
