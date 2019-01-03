3633a800750e321bfe46b7a42a1cbd5538b0955b
Fabio <heloflyer@hotmail.com>
2019-01-02 19:15:07 -0800

    completed add-edit form, connected with redux to edit and remove, moved remove button from ExpenseListItem to EditExpensePage

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,10 +5,13 @@ import filtersReducer from '../reducers/filters';
 // Store Creation
 
 export default () => {
-    const store = createStore(combineReducers({
-        expenses: expensesReducer,
-        filters: filtersReducer
-    }));
+    const store = createStore(
+        combineReducers({
+            expenses: expensesReducer,
+            filters: filtersReducer
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 
     return store;
 };
