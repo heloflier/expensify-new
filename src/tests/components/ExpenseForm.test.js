23877775b0aa4a398c270fdf172943b8bedb20b4
Fabio <heloflyer@hotmail.com>
2019-01-08 18:34:05 -0800

    set up test for AddExpensePage

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -85,7 +85,6 @@ test('should set new date on date change', () => {
 test('should set calendar focus on change', () => {
     const focused =  true;
     const wrapper = shallow(<ExpenseForm />);
-    console.log('----------------------- ', {focused})
     wrapper.find('SingleDatePicker').prop('onFocusChange')({ focused });
     expect(wrapper.state('calendarFocused')).toBe(focused);
 });
