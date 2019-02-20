2d6c4d7680c7b7d4965a0e9979e06dfd1bc8e63f
Fabio <heloflyer@hotmail.com>
2019-02-19 17:23:19 -0800

    added action and test for startsetexpenses

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -51,8 +51,8 @@ export const startAddExpense = (expenseData = {}) => {
             amount = 0,
             createdAt = 0
         } = expenseData;
+        const expense = { description, note, amount, createdAt };
 
-        const expense = { description, note, amount, createdAt }
         return database.ref('expenses').push(expense).then((ref) => {
             dispatch(addExpense({
                 id: ref.key,
@@ -74,3 +74,36 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+// START_SET_EXPENSES
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        // const {
+        //     description = '',
+        //     note = '',
+        //     amount = 0,
+        //     createdAt = 0
+        // } = startExpenses;
+        // const expense = { description, note, amount, createdAt };
+
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                })
+            })
+            console.log(expenses);
+
+            dispatch(setExpenses(expenses));
+        });
+    };
+};
